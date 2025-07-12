57b470e50e782e5b7296d528eef8e6133596aa83
hyeonseo1109 <alsgustj2@naver.com>
2025-07-12 14:16:14 +0900

    key파일 수정

---
+++ src/key.js
@@ -0,0 +1,7 @@
+const key = "5336e784ac078780e036e421663adbea";
+
+export default key;
+
+
+
+//https://api.openweathermap.org/data/2.5/forecast?lat=37.62684989552039&lon=126.84168623558315&units=metric&lang=kr&appid=5336e784ac078780e036e421663adbea
